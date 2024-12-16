d9f4512b2463e5fdb4cb39e228ff24c1062a4965
vishnujayee <jayeevishnu8@gmail.com>
2024-12-16 18:47:10 +0530

    change backend url from localhost to deployurl

--- fronteend/src/services/moreapi.js
+++ fronteend/src/services/moreapi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const moreapi = createApi({
     reducerPath: "moreapi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:8080"
+        baseUrl: "https://shop-eoo8-gcfa1tlhf-vishnujayees-projects.vercel.app/"
     }),
     endpoints: (builder) => ({
         getorders:builder.mutation({
