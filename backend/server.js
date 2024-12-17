f31310a46dbfac3c90a0b2a38e7932af1501fddf
vishnujayee <jayeevishnu8@gmail.com>
2024-12-17 09:29:46 +0530

    issue-cors

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,21 @@ const sellerroute = require("../backend/routes/seller_dahboard_routes");
 require('./connecttion');
 const cors = require('cors');
 const app = express();
-app.use(cors());
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
+  }
+app.use(cors(allowCors));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 // app.use(checkAuth);
