981a9ca5313c919345e08a5e4f14c075c512b0b2
vishnujayee <jayeevishnu8@gmail.com>
2024-12-20 07:14:27 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
     origin: '*', 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization' ,'SellerAuth' , 'authorization']
+    allowedHeaders: ['Content-Type', 'Authorization' ]
 }));
 app.options('*', cors());
 app.use(express.urlencoded({ extended: false }));
